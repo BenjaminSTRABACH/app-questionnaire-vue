662e9bada5aa8dc5fd986b15cf4015e957e74c55
Unknown <benjamin.strabach@live.fr>
2019-10-15 13:37:31 +0200

    Ajout des fichiers du projet

---
+++ src/components/Home.vue
@@ -0,0 +1,41 @@
+<template>
+    <div>
+        <h1>Bienvenue sur l'application</h1>
+        <form novalidate class="md-layout" @submit.prevent="validateUser">
+            <md-card class="md-layout-item md-size-50 md-small-size-100">
+                <md-card-header>
+                    <div class="md-title">Connexion</div>
+                </md-card-header>
+                <md-card-content>
+                    <div class="md-layout md-gutter">
+                        <div class="md-layout-item md-small-size-100">
+                            <!-- <md-field :class="getValidationClass('firstName')"> -->
+
+                            <!-- </md-field> -->
+                        </div>
+                    </div>
+                </md-card-content>
+            </md-card>
+        </form>
+    </div>
+</template>
+
+<script>
+    import { validationMixin } from 'vuelidate'
+
+    export default {
+        name: 'Home',
+        
+        
+        // validations: {
+        //     form: {
+        //         firstName: {
+        //         required,
+        //         minLength: minLength(3)
+        //         }
+        //     }
+        // },
+    }
+
+    
+</script>
