264a663916b6a5761fcc7754a3e4f5fc5bdb5c57
Unknown <benjamin.strabach@live.fr>
2019-11-17 15:26:35 +0100

    Affichage de toutes les questions
    Cochage des cases non fonctionnel

--- src/views/Questionnaire.vue
+++ src/views/Questionnaire.vue
@@ -1,40 +1,46 @@
 <template>
-    <div>
-        <form novalidate class="md-layout" @submit.prevent="validateAnswer">
-            <md-card class="md-layout-item md-size-100 md-small-size-100">
-                <md-card-header>
-                    <div class="md-title">{{ q.Questions[0].Libelle }}</div>
-                </md-card-header>
-                <md-card-content>
-                    
-                    <!-- Affichage des réponses en fonction de la question -->
-                    <!-- for -->
-                    <md-checkbox v-model="answer" v-for="index in q.Questions[0].Answers" v-bind:key="index">{{index.Libelle}}</md-checkbox>
-                    <!-- fin for -->
-
-                </md-card-content>
-                <md-card-actions>
-                    <md-button type="submit" class="md-raised" :disabled="sending">Valider</md-button>
-                </md-card-actions>
-            </md-card>
-        </form>
+    <div class="question">
+        <img src="@/assets/question.png" class="question_logo">
+        <!-- Contenu de la question -->
+        <md-card class="md-layout-item md-size-100 md-small-size-100" v-for="(Id, i) in ql.Questions" v-bind:key="Id[Id]">
+            <!-- Intitulé de la question en titre -->
+            <md-card-header>
+                <div class="md-title">{{ ql.Questions[i].Libelle }}</div>
+            </md-card-header>
+            <!-- Affichage des réponses en fonction de la question -->
+            <md-card-content>
+                <md-checkbox v-model="answer[j]" v-for="(Id, j) in ql.Questions[i].Answers" v-bind:key="Id[Id]">{{Id.Libelle}}</md-checkbox>
+            </md-card-content>
+            <!-- Validation -->
+            <md-card-actions>
+                <md-button type="submit" class="md-raised">Valider</md-button>
+            </md-card-actions>
+        </md-card>
     </div>
 </template>
 
 <script>
     import { validationMixin } from 'vuelidate'
-    // import {
-    //     required
-    // } from 'vuelidate/lib/validators'
     import questionsList from "@/assets/questions.json"
 
     export default {
         name: 'Questionnaire',
         mixins: [validationMixin],
         data: () => ({
-            q: questionsList
+            ql: questionsList,
+            answer: []
         })
     }  
 
 
 </script>
+
+<style>
+.question_logo{
+    margin: 30px;
+    width: 400px;
+    height: 400px;
+}
+
+
+</style>
